f3557a36c28b32986e887898e6309202de93050d
Subhankar-Ray192 <subhankarraypp32@gmail.com>
2023-11-09 10:51:58 +0530

    Updated

--- threading/posix_thread.c
+++ threading/posix_thread.c
@@ -20,7 +20,7 @@ void *exec_thread(void *args)
 {
 	printf("Child-Thread-Start\n");
 	printf("Message:%s\n",(char *)args);
-	sleep(3);
+	sleep(5);
 	strcpy(msg,"Bye");
 	printf("New-Message:%s\n",msg);
 	pthread_exit("Child Thread Joined");
@@ -29,10 +29,10 @@ void *exec_thread(void *args)
 void main_thread()
 {
 	printf("\nMain-Thread-Start\n");
+	sleep(3);
 	pthread_create(&obj.T,NULL,exec_thread,(void *)msg);
 	pthread_join(obj.T,&stat);
 	printf("%s\n",(char *)stat);
-	sleep(3);
 	printf("Main-Thread-Finish\n");
 }
 
