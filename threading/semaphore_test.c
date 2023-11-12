d1c35a77e2e82ec678d1d5cbb596a4d333d9a4b2
Subhankar-Ray192 <subhankarraypp32@gmail.com>
2023-11-12 18:28:17 +0530

    Semaphore-Threading-ID Added

--- threading/semaphore_test.c
+++ threading/semaphore_test.c
@@ -13,6 +13,7 @@ struct semaphore
 struct thread
 {
 	pthread_t T;
+	pthread_t ID;
 };
 
 void * stat;
@@ -29,6 +30,7 @@ void *exe_thread(void *args)
 	sem_wait(&obj.S);
 	shared_res++;
 	printf("Message:%s\n",(char *)args);
+	sleep(3);
 	sem_post(&obj.S);
 	pthread_exit("Finished");
 }
@@ -39,13 +41,16 @@ void main_thread()
 	struct thread obj2;
 
 	pthread_create(&obj1.T,NULL,exe_thread,(void *)msg);
+	obj1.ID=pthread_self();
+	
 	pthread_create(&obj2.T,NULL,exe_thread,(void *)msg);
+	obj2.ID=pthread_self();
 
 	pthread_join(obj1.T,&stat);
-	printf("Status:%s\n",(char *)stat);
+	printf("%d-Status:%s\n",obj1.ID,(char *)stat);
 	
 	pthread_join(obj2.T,&stat);
-	printf("Status:%s\n",(char *)stat);
+	printf("%d-Status:%s\n",obj2.ID,(char *)stat);
 	
 	printf("Shared-Resouce:%d\n",shared_res);
 }
