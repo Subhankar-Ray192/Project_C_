9fdf7a297077ab962b03dbc3528bcd641f034faa
Subhankar-Ray192 <subhankarraypp32@gmail.com>
2023-11-12 14:51:00 +0530

    Semaphore Added

---
+++ threading/semaphore_test.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<semaphore.h>
+#include<pthread.h>
+#include<string.h>
+
+struct semaphore
+{
+	sem_t S;
+};
+
+struct thread
+{
+	pthread_t T;
+};
+
+void * stat;
+int shared_res=0;
+char msg[]={"Hello World!"};
+
+struct semaphore obj;
+
+void *exe_thread(void *args);
+void main_thread();
+
+void *exe_thread(void *args)
+{
+	sem_wait(&obj.S);
+	shared_res++;
+	printf("Message:%s\n",(char *)args);
+	pthread_exit("Finished");
+	sem_post(&obj.S);
+}
+
+void main_thread()
+{
+	struct thread obj1;
+	struct thread obj2;
+
+	pthread_create(&obj1.T,NULL,exe_thread,(void *)msg);
+	pthread_join(obj1.T,&stat);
+	printf("Status:%s\n",(char *)stat);
+
+	pthread_create(&obj2.T,NULL,exe_thread,(void *)msg);
+	pthread_join(obj2.T,&stat);
+	printf("Status:%s\n",(char *)stat);
+	
+	printf("Shared-Resouce:%d\n",shared_res);
+}
+
+void main()
+{
+	sem_init(&obj.S,0,2);
+	main_thread();
+}
