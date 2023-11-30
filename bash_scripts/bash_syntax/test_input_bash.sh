edc33bba10554a874004ed8eed1ccdb67c804c03
Subhankar-Ray192 <subhankarraypp32@gmail.com>
2023-11-30 18:57:40 +0530

    Updated Code

--- bash_scripts/bash_syntax/test_input_bash.sh
+++ bash_scripts/bash_syntax/test_input_bash.sh
@@ -1,18 +1,17 @@
 
+#SHELL-COMMAND-LINE-INPUT
 
-	#SHELL-COMMAND-LINE-INPUT
+var_1=$1
+echo "TEST-1:" $var_1
 
-	var_1=$1
-	echo "TEST-1:" $var_1
+#SHELL-READ-INPUT:
 
-	#SHELL-READ-INPUT:
+echo -n "NON-PROMTED:"
+read var_2
+echo "TEST-2.0:" $var_2
 
-	echo -n "NON-PROMTED:"
-	read var_2
-	echo "TEST-2.0:" $var_2
-
-	read -p "PROMPTED:" var_3
-	echo "TEST-2.1:" $var_3
+read -p "PROMPTED:" var_3
+echo "TEST-2.1:" $var_3
 
 
 if [ "$BASH" ]; then
