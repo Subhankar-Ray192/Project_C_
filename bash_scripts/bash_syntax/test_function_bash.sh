b2ce661ea9dbcf62b41039e2fb0d7c8d22bd6922
Subhankar-Ray192 <subhankarraypp32@gmail.com>
2023-12-04 03:27:41 +0530

    Added functions and strings for shell(local)

--- bash_scripts/bash_syntax/test_function_bash.sh
+++ bash_scripts/bash_syntax/test_function_bash.sh
@@ -1,3 +1,37 @@
 #SHELL-FUNCTIONS
 
+test_function()
+{
+	echo "TESTING:FUNCTION"
+}
+
+test_function_1()
+{
+	read -p "ENTER NUMBER:" var_1
+	echo "TESTING:FUNCTION(PARAMS)-->" $var_1
+}
+
+var_2=""
+
+test_function_2()
+{
+	read -p "ENTER STRING:" var_2
+	var_2=$(echo $var_2| cut -d' ' -f1,3)
+}
+
+
+test_function_3()
+{
+	read -p "ENTER NUMBER:" var_3
+	echo $var_3
+}
+
+test_function
+test_function_1
+
+test_function_2
+echo "TESTING:FUNCTION(PARAMS)-->RETURNS>>" $var_2 
+
+return_val=$(test_function_3)
+echo "TESTING:FUNCTION(PARAMS)-->RETURNS>>" $return_val
 #BASH-FUNCTIONS
--- bash_scripts/bash_syntax/test_string_bash.sh
+++ bash_scripts/bash_syntax/test_string_bash.sh
@@ -1,5 +1,44 @@
 #SHELL-STRING-FUNCTION
 
+my_string="Hello World!"
+
+echo
+echo "INPUT-STRING:" $my_string
+
+echo
+#DELIMITER-ROW-FIELD-EXTRACTION
+substring=$(echo $my_string| cut -d' ' -f2)
+echo "R-FIELD:" $substring
+
+#DELIMETER-COLUMN-FIELD-EXTRACTION
+substring=$(echo $my_string| cut -c2-7)
+echo "C-FIELD(2,7):" $substring
+
+#DELIMETER-COLUMN-SPECIFIC-CHARACTERS-EXTRACTION
+substring=$(echo $my_string| cut -c3,5,8)
+echo "C-FIELD(3,5,8):" $substring
+
+my_string_3="apple:orange:mango"
+
+#DELIMITER-ROW-SPECIFIC-CHARACTER-EXTRACTION
+substring=$(echo $my_string_3| cut -d':' -f1,3)
+echo "R-FIELD(1,3):" $substring
+
+echo
+#my_string_1="apple,orange,banana"
+#awk -F',' '{ for(i=1;i<=NF;i++) print $i }' <<<$my_string_1
+
+if echo "$my_string" | grep -q "Hello"; then
+	echo "String contains Hello"
+fi
+
+echo
+uppercase=$(echo $my_string | tr '[:lower:]' '[:upper:]')
+lowercase=$(echo $my_string | tr '[:upper:]' '[:lower:]')
+
+echo "Uppercase:" $uppercase
+echo "Lowercase:" $lowercase
+
 #BASH-STRING-FUNCTION
 
 
